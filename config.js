ac3a34e64e3a228668f32d9dbf46b26dde4af759
MrLloyd808 <127343644+MrLloyd808@users.noreply.github.com>
2023-03-11 17:09:11 +0200

    Update config.js
    Signed-off-by: MrLloyd808 <127343644+MrLloyd808@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,12 +37,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
+global.owner = ['27799400929','']
+global.premium = ['27799400929']
 global.botname = 'ZIM BOT INC'
 global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
+global.group1 = 'https://chat.whatsapp.com/EFsbRCXV4jLEFk4eAcA1A'
+global.group2 = 'https://chat.whatsapp.com/HSfcYU135C8GxINwSWoA'
 global.packname = 'ZIM BOT INC'
 global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
 global.monayawal = '100000'
